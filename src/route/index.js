901a5c2193e29acd2407899523239835408ae8f6
Игорь <innkoroleva2017@mail.com>
2023-05-04 08:38:05 +0300

    upd packages

--- src/route/index.js
+++ src/route/index.js
@@ -985,7 +985,7 @@ router.get('/task22', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('bootstrap4', function (req, res) {
+router.get('/bootstrap4', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
